8a50790a04287163399d522e0ae8eed344f5fce4
juandavidcertuche93 <juandavidcertuchequijano2@gmail.com>
2023-12-28 17:42:37 -0500

    primer commit creacion de los modulos, componentes, paginas etc

---
+++ src/app/modules/user/pages/users-table/users-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './users-table.component.html'
+})
+export class UsersTableComponent {
+
+}
