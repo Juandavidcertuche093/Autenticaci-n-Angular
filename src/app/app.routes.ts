e130bc527633c8914ca10cd6e9401197ce9161d3
juandavidcertuche93 <juandavidcertuchequijano2@gmail.com>
2024-01-03 08:55:12 -0500

    quinto commit creacion de las users

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,11 @@ export const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./modules/auth/auth.module').then((m) => m.AuthModule),
-  }  
+  },
+  {
+    path: 'app',
+    loadChildren: () => import('./modules/layout/layout.module').then((m) => m.LayoutModule),
+  }, 
 ];
 
 @NgModule({
@@ -14,11 +18,3 @@ export const routes: Routes = [
 })
 export class AppRoutingModule { }
 
-/* 
-  
-
-  {
-    path: 'app',
-    loadChildren: () => import('./modules/layout/layout.module').then((m) => m.LayoutModule),
-  },
-  */
